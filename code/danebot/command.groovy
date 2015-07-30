24d0151bf3fd30c6de6b2ba75beb0c03b1ef2b04
Craig Burke <craig@craigburke.com>
2015-07-30 15:30:11 -0500

    Publish of Github pages from Gradle.

--- code/danebot/command.groovy
+++
@@ -1,5 +0,0 @@
-'Dane Cook sucks'.watch { tweetedStatus ->
-    println "@${tweetedStatus.user.screenName} tweeted: ${tweetedStatus.text}"
-    tweet 'Hey @DaneCook, somebody said this:' << tweetedStatus
-    tweet "LEAVE DANE ALONE, @${tweetedStatus.user.screenName}!!!"
-}
--- code/doc-builder/command.groovy
+++
@@ -1,3 +0,0 @@
-document {
-	paragraph 'Hello World'
-}
